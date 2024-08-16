3445fb308f9c3166febed623134552c77034b4fc
Abhinav Chauhan <abhinav.chauhan.code@gmail.com>
2024-08-16 22:38:04 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://fuzzzy.onrender.com", {
+			const socket = io("https://fusssy.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
